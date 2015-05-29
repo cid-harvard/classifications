7d5b3b11d28554d09d995c35cca500b38bf832ae
Mali Akmanalp <mali@akmanalp.com>
2015-05-29 16:10:30 -0400

    Remove code that was replaced by classification stuff

--- industry/ISIC/Colombia/read_dane_table.py
+++ industry/ISIC/Colombia/read_dane_table.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 
 import pandas as pd
 import numpy as np
 
-import os.path
 import sys
 
 
@@ -69,75 +67,6 @@ def parse_dane(df):
     return df
 
 
-def get_parent_code(row_type, hierarchy):
-    """Given a list that defines a hierarchy from highest to lowest level, find
-    the parent of the given level."""
-    try:
-        current_level = hierarchy.index(row_type)
-    except ValueError:
-        raise ValueError("Hierarchy level {} does not exist. Valid: {}",
-                         row_type, hierarchy)
-
-    if current_level == 0:
-        return None
-
-    parent_level = current_level - 1
-    return hierarchy[parent_level]
-
-
-def set_parents(df, hierarchy):
-    """Go through a parsed classification spreadsheet and infer the hierarchy
-    from the order of the items. Assumed: a lower level item must be the child
-    of the previous higher level item."""
-
-    hier_index = dict(zip(hierarchy, [None] * 4))
-
-    df["parent"] = np.NaN
-
-    def traversal_iteration(x):
-        hier_index[x.type] = x.code
-        parent_code = get_parent_code(x.type, hierarchy)
-        if parent_code is not None:
-            x.parent = hier_index[parent_code]
-        return x
-
-    df = df.apply(traversal_iteration, axis=1)
-    return df
-
-
-def filter_by_hierarchy_level(df, levels):
-    return df[df.type.isin(levels)]
-
-
-def build_aggregation_table(df, start_level, end_level, hierarchy):
-    """Build a table from one level of a hierarchy to another, like 2digit to
-    4digit."""
-
-    assert(start_level in hierarchy)
-    assert(end_level in hierarchy)
-
-    start_index = hierarchy.index(start_level)
-    end_index = hierarchy.index(end_level)
-
-    assert(start_index < end_index)
-
-    lookup_table = df[["code", "type", "parent"]].set_index("code").copy()
-    parents = df.loc[df.type == end_level, ["code", "parent"]]
-
-    def update(x):
-        x.parent = lookup_table.loc[x.parent].parent
-        return x
-
-    for _ in range(end_index - start_index - 1):
-        parents = parents.apply(update, axis=1)
-
-    parents = parents.set_index("code")
-    parents.columns = [start_level]
-    parents.index.name = end_level
-
-    return parents
-
-
 if __name__ == "__main__":
     assert(len(sys.argv) == 3)
 
@@ -148,14 +77,14 @@ if __name__ == "__main__":
     df = parse_dane(df)
     df = df[~df.duplicated(["code"])]
     df = df.reset_index(drop=True)
-    df = set_parents(df, DANE_HIERARCHY)
+    df.columns = ["name", "level", "code"]
 
-    file_prefix = os.path.basename(file_name)
+    from classification import (parent_code_table_to_parent_id_table,
+                                Classification, Hierarchy,
+                                ordered_table_to_parent_code_table)
 
-    df.columns = ["name", "level", "code", "parent_code"]
-
-    from classification import parent_code_table_to_parent_id_table, Classification, Hierarchy
     h = Hierarchy(DANE_HIERARCHY)
+    df = ordered_table_to_parent_code_table(df, h)
     df = parent_code_table_to_parent_id_table(df, h)
     c = Classification(df, h)
 
