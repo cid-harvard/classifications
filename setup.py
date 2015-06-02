7f15a7800cc2a422c938d4590cb3754b36680048
Mali Akmanalp <mali@akmanalp.com>
2015-06-02 18:06:36 -0400

    Add colombia locations into setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
             'industry/ISIC/Colombia/out/isic_ac_3.0.csv',
             'industry/ISIC/Colombia/out/isic_ac_4.0.csv',
             'product/HS/Atlas/out/hs92_atlas.csv',
+            'location/Colombia/DANE/out/locations_colombia_dane.csv',
         ]
     }
 )
