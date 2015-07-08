9a00870258cfca574402f90463cba3c9b44ec165
Mali Akmanalp <mali@akmanalp.com>
2015-07-08 16:00:29 -0400

    Update setup.py to include mexico locations

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="linnaeus",
-    version="v0.0.1",
+    version="v0.0.8",
     author="Mali Akmanalp <Harvard CID>",
     description=("Harvard CID's classification tools."),
     url="http://github.com/cid-harvard/classifications/",
@@ -16,6 +16,7 @@ setup(
             'industry/ISIC/Colombia/out/isic_ac_4.0.csv',
             'product/HS/Atlas/out/hs92_atlas.csv',
             'location/Colombia/DANE/out/locations_colombia_dane.csv',
+            'location/Mexico/INEGI/out/locations_mexico_inegi.csv',
         ]
     }
 )
