0aa6f2779313e3224250abb8465b662a2c4c9ed9
Mali Akmanalp <mali@akmanalp.com>
2015-07-21 16:23:18 -0400

    Add SINCO

---
+++ occupation/SINCO/Mexico/README.md
@@ -0,0 +1,4 @@
+Sistema Nacional de Clasificación de Ocupaciones (SINCO) 2011
+
+http://www3.inegi.org.mx/sistemas/clasificaciones/sinco/sinco.aspx
+
---
+++ occupation/SINCO/Mexico/clean.py
@@ -0,0 +1,33 @@
+# vim: set fileencoding=utf8 :
+
+import pandas as pd
+
+from classification import (Hierarchy, ordered_table_to_parent_code_table,
+                            parent_code_table_to_parent_id_table,
+                            Classification)
+
+if __name__ == "__main__":
+
+    sinco = pd.read_csv("in/SINCO_2011.csv", header=None, encoding="latin-1")
+
+    sinco.columns = ["data"]
+    sinco = sinco[~sinco.data.str.startswith("INEGI.")]
+    sinco = sinco[~sinco.data.str.startswith(u"Clave Descripción")]
+
+    for index, row in reversed(list(sinco[~sinco.data.str.match("^\d* ")].iterrows())):
+        sinco.ix[index - 1] += (" " + sinco.ix[index])
+
+    sinco = sinco[sinco.data.str.match("^\d* ")]
+
+    sinco = sinco.data.str.split(" ", 1).apply(pd.Series, 1)
+    sinco.columns = ["code", "name"]
+
+    sinco["level"] = sinco["code"].apply(lambda x: str(len(x)) + "digit")
+    h = Hierarchy(["1digit", "2digit", "3digit", "4digit"])
+
+    parent_code_table = ordered_table_to_parent_code_table(sinco, h)
+    parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
+
+    c = Classification(parent_id_table, h)
+
+    c.to_csv("out/occupations_sinco_2011.csv")
