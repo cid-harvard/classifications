e690ed1e42f20baf539648c30d9f4eccf96f36d6
Mali Akmanalp <mali@akmanalp.com>
2015-07-23 18:49:43 -0400

    Add Mexico prospedia HS classification

---
+++ product/HS/Mexico_Prospedia/README.md
@@ -0,0 +1,9 @@
+Original data from INEGI: Aspectos Normativos y Metodologicos. Tabla de
+Correlación entre la Tarifa de la Ley de los Impuestos Generales de Importación
+y de Exportación (TIGIE) y el Sistema de Clasificación Industrial de América
+del Norte (SCIAN). Actualización 2013
+
+Main page: http://www3.inegi.org.mx/sistemas/clasificaciones/tigie/tigie.aspx
+Select all items, hit “Ver Consulta En XLS”.
+
+Names generously translated / shortened by Ryan MacLennan by hand, [here](https://docs.google.com/spreadsheets/d/13fowekaxh5QdbtBumMv_CMzk3APwHzSRZeAOyBc46sk/edit#gid=0).
---
+++ product/HS/Mexico_Prospedia/clean.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+from classification import (Hierarchy, repeated_table_to_parent_id_table,
+                            parent_code_table_to_parent_id_table,
+                            Classification)
+
+if __name__ == "__main__":
+
+    trans = pd.read_csv("./in/HS4_Spanish_English_Translations.csv", encoding="utf-8")
+    trans = trans[["code", "level", "name_es", "name_en", "name_short_es", "name_short_en"]]
+
+    def fill_code(x):
+        if x.level == "4digit":
+            x.code = str(x.code).zfill(4)
+        if x.level == "2digit":
+            x.code = str(x.code).zfill(2)
+        if x.level == "section":
+            x.code = str(x.code)
+        if x.level == "prospedia_section":
+            x.code = str(x.code).zfill(1)
+        return x
+
+    trans = trans.apply(fill_code, axis=1)
+
+    # Prospedia specific
+    trans = trans[trans.level != "section"]
+    df = pd.read_table("./in/prospedia_hs_structure.txt")
+    df.columns = ["4digit_code", "2digit_code", "prospedia_section_code"]
+    df["4digit_code"] = df["4digit_code"].astype(str).str.zfill(4)
+    df["4digit_name"] = None
+    df["2digit_code"] = df["2digit_code"].astype(str).str.zfill(2)
+    df["2digit_name"] = None
+    df["prospedia_section_name"] = None
+    df["prospedia_section_code"] = df["prospedia_section_code"].astype(str).str.zfill(1)
+
+    h = Hierarchy(["prospedia_section", "2digit", "4digit"])
+
+    parent_code_table = repeated_table_to_parent_id_table(df, h)
+    parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
+
+    parent_id_table = parent_id_table.merge(trans, on=["level", "code"])
+    parent_id_table.name = parent_id_table.name_en
+
+    assert parent_id_table.name.isnull().sum() == 3
+    parent_id_table.loc[parent_id_table.name.isnull(), "name"] = u"No name"
+    assert parent_id_table.name.isnull().sum() == 0
+
+    c = Classification(parent_id_table, h)
+
+    c.to_csv("out/products_mexico_prospedia.csv")
+    c.to_stata("out/products_mexico_prospedia.dta")
