bda12d19851c42269aeb346ca124ca7f984a23eb
Mali Akmanalp <mali@akmanalp.com>
2015-08-03 15:27:35 -0400

    Switch default country names to english, fill missing english names

--- location/International/DANE/clean.py
+++ location/International/DANE/clean.py
@@ -7,18 +7,34 @@ from classification import (Hierarchy, repeated_table_to_parent_id_table,
 if __name__ == "__main__":
 
     df = pd.read_stata("in/Colombia_countries_key.dta", encoding="mac-roman")
-    df.columns = ["alpha3", "code", "name"]
+    df.columns = ["alpha3", "code", "name_es"]
 
-    df = df[["alpha3", "code", "name"]]
+    df = df[["alpha3", "code", "name_es"]]
 
     df["parent_code"] = pd.np.nan
     df["level"] = "country"
 
     iso = pd.read_csv("../iso/in/wikipedia-iso-country-codes.csv", encoding="utf-8")
     iso = iso[["Alpha-3 code", "English short name (upper/lower case)"]]
-    iso.columns = ["alpha3", "name_en"]
+    iso.columns = ["alpha3", "name"]
 
     df = df.merge(iso, on="alpha3", how="left")
+
+    assert df[df.name.isnull()].shape[0] == 4
+
+    assert df.loc[7, "name"] is pd.np.nan
+    df.loc[7, "name"] = "Netherlands Antilles"
+
+    assert df.loc[192, "name"] is pd.np.nan
+    df.loc[192, "name"] = "Serbia and Montenegro"
+
+    assert df.loc[249, "name"] is pd.np.nan
+    df.loc[249, "name"] = "Not Declared"
+
+    assert df.loc[250, "name"] is pd.np.nan
+    df.loc[250, "name"] = "Special Economic Zones"
+
+    df.name = df.name.astype(unicode)
 
     h = Hierarchy(["country"])
 
