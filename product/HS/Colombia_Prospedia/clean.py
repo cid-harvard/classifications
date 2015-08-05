4e5f1f2ee1f39e3f9863ddcad13fa9edb83c5a70
Mali Akmanalp <mali@akmanalp.com>
2015-08-05 18:39:33 -0400

    Change prosperia_section to section

--- product/HS/Colombia_Prospedia/clean.py
+++ product/HS/Colombia_Prospedia/clean.py
@@ -5,23 +5,24 @@ from classification import (Hierarchy, repeated_table_to_parent_id_table,
                             Classification)
 
 if __name__ == "__main__":
-    names = pd.read_table("./in/HS_hierarchy_master - Names.tsv", encoding="utf-8", dtype={"code": str})
+    names = pd.read_table("./in/HS_hierarchy_master - Names.tsv",
+                          encoding="utf-8", dtype={"code": str})
 
     hierarchy = pd.read_table("./in/HS_hierarchy_master - Hierarchy.tsv",
                               encoding="utf-8",
                               dtype={
                                   "4digit": str,
                                   "2digit": str,
-                                  "prosperia_section": str,
                                   "section": str,
+                                  "atlas_section": str,
                               })
-    hierarchy.columns = ["4digit_code", "2digit_code", "prosperia_section_code", "atlas_section"]
+    hierarchy.columns = ["4digit_code", "2digit_code", "section_code", "atlas_section"]
     hierarchy["4digit_name"] = None
     hierarchy["2digit_name"] = None
-    hierarchy["prosperia_section_name"] = None
+    hierarchy["section_name"] = None
 
 
-    h = Hierarchy(["prosperia_section", "2digit", "4digit"])
+    h = Hierarchy(["section", "2digit", "4digit"])
     parent_code_table = repeated_table_to_parent_id_table(hierarchy, h)
 
     parent_code_table.code = parent_code_table.code.astype(str)
