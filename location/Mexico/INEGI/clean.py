9677d4c2f37d87bdbb9c3493af776b84db8cabfc
Mali Akmanalp <mali@akmanalp.com>
2015-09-21 16:05:57 -0400

    Mexico locations changes: - Add mex level - rename state level to department - get rid of locality level

--- location/Mexico/INEGI/clean.py
+++ location/Mexico/INEGI/clean.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
              "municipality_name", "locality_code",
              "locality_name"]]
 
-
     df.state_code = df.state_code.astype(str).str.zfill(2)
     df.municipality_code = df.municipality_code.astype(str).str.zfill(3)
     df.locality_code = df.locality_code.astype(str).str.zfill(4)
@@ -33,11 +32,43 @@ if __name__ == "__main__":
     df.municipality_name = df.municipality_name.str.title()
     df.locality_name = df.locality_name.str.title()
 
+    df = df.rename(columns={"state_name": "name_en_state",
+                            "municipality_name": "name_en_municipality",
+                            "locality_name": "name_en_locality",
+                            })
+
     h = Hierarchy(["state", "municipality", "locality"])
 
-    parent_code_table = repeated_table_to_parent_id_table(df, h)
+    parent_code_table = repeated_table_to_parent_id_table(
+        df, h,
+        level_fields={
+            "state": ["name_en_state"],
+            "municipality": ["name_en_municipality"],
+            "locality": ["name_en_locality"],
+        }
+    )
+
+    # TODO: This isn't the official classification level name but this makes
+    # compatibility between colombia and mexico way easier
+    parent_code_table.loc[parent_code_table.level == "state", "level"] = "department"
+
+    # Drop the "locality" level since we don't use it
+    parent_code_table = parent_code_table[parent_code_table.level != "locality"]
+
+    # This adds a highest level element that represents the whole country
+    mex = pd.Series({
+        "code": "MEX",
+        "name_en": "Mexico",
+        "level": "country"
+    })
+    parent_code_table.loc[parent_code_table.level == "department", "parent_code"] = "MEX"
+    parent_code_table = pd.concat([pd.DataFrame(mex).T, parent_code_table])
+    parent_code_table = parent_code_table.reset_index(drop=True)
+
+    h = Hierarchy(["country", "department", "municipality"])
     parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
 
+    parent_id_table["name"] = parent_id_table["name_en"]
     c = Classification(parent_id_table, h)
 
     c.to_csv("out/locations_mexico_inegi.csv")
