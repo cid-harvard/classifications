a9b79059e8809b0d8a88ed68452d56106e790830
Mali Akmanalp <mali@akmanalp.com>
2015-10-19 15:53:44 -0400

    Add datlas version of mexico industry classification with top level changed, courtesy of Tim.

---
+++ industry/NAICS/Mexico_datlas/README.md
@@ -0,0 +1,14 @@
+Original data from INEGI: Sistema de Clasificación Industrial de América del Norte
+
+
+Main page: http://www.inegi.org.mx/est/contenidos/proyectos/SCIAN/presentacion.aspx
+[2013 Table](http://www.inegi.org.mx/est/contenidos/proyectos/SCIAN/presentacion.aspx?_file=/est/contenidos/proyectos/SCIAN/doc/est_ingles.xlsx)
+[2007 Table](http://www.inegi.org.mx/sistemas/scian/contenidos/ESTRUCTURA%20SCIAN%20M%C9XICO%202007%20TRADUCCI%D3N%20INGL%C9S.xlsx)
+
+Raw files were hand-fixed because INEGI put in a category named 31-33, when
+they should have put in 3 categories. So I inserted those new categories and
+named them Manufacturing Industries 1, 2, 3 etc.
+
+Also, this "datlas" version has the top level categories changed by Tim Cheston, work for that is here:
+
+https://docs.google.com/spreadsheets/d/1lm1ta4cJZfpPx2VJsOOJMqlf9_mrd8VdNxTbVeHOzwc/edit#gid=1506832830
---
+++ industry/NAICS/Mexico_datlas/clean.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf8 :
+
+import pandas as pd
+
+from classification import (Hierarchy, repeated_table_to_parent_id_table,
+                            parent_code_table_to_parent_id_table,
+                            Classification)
+
+if __name__ == "__main__":
+
+    hierarchy = pd.read_table("in/Mexico_industry_master - Hierarchy.tsv")
+
+    fields = {
+        "section": [],
+        "division": [],
+        "group": [],
+        "class": [],
+    }
+
+    h = Hierarchy(["section", "division", "group", "class"])
+    parent_code_table = repeated_table_to_parent_id_table(hierarchy, h, fields)
+
+    # Workaround for Timageddon issue - Tim made some codes like 31 / 32 / 33
+    # fall under different sections sometimes, and this breaks the strict
+    # treelike hierarchy of the classification. This happens mainly at the
+    # division level. We've decided to cut our losses right now and pick one.
+    # This comes at the expense of possibly mis-assigning some class level
+    # codes to different codes but until someone either fixes the
+    # classification or the classification system entirely, there's not much we
+    # can do here.
+
+    parent_code_table = parent_code_table[~parent_code_table.duplicated(["code", "level"])]
+    parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
+
+    names = pd.read_table("in/Mexico_industry_master - Names.tsv", encoding="utf-8")
+    parent_id_table = parent_id_table.merge(names, on=["code", "level"], how="outer")
+
+    parent_id_table["name"] = parent_id_table["name_en"]
+    parent_id_table.code = parent_id_table.code.astype(str)
+
+    c = Classification(parent_id_table, h)
+
+    c.to_csv("out/industries_mexico_scian_2007_datlas.csv")
+    c.to_stata("out/industries_mexico_scian_2007_datlas.dta")
