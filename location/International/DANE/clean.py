fd53011a9e3fdf721e33a436377b6243a1b796b2
Ryan MacLennan <maclennanryan@gmail.com>
2015-11-06 17:33:53 -0500

    Updated colombia country export file with region codes and heirarchy

--- location/International/DANE/clean.py
+++ location/International/DANE/clean.py
@@ -11,36 +11,42 @@ if __name__ == "__main__":
 
     df = df[["alpha3", "code", "name_es"]]
 
-    df["parent_code"] = pd.np.nan
-    df["level"] = "country"
-
     iso = pd.read_csv("../iso/in/wikipedia-iso-country-codes.csv", encoding="utf-8")
     iso = iso[["Alpha-3 code", "English short name (upper/lower case)"]]
     iso.columns = ["alpha3", "name"]
 
     df = df.merge(iso, on="alpha3", how="left")
 
-    assert df[df.name.isnull()].shape[0] == 4
+    mex = pd.read_csv("../Mexico/Mexico Country codes - continents - Countries.csv")
+    mex = mex[["code", "name_es", "name_short_es", "continent_code"]]
 
-    assert df.loc[7, "name"] is pd.np.nan
-    df.loc[7, "name"] = "Netherlands Antilles"
+    df = df.merge(mex, how = "left", left_on = "alpha3", right_on = "code", suffixes=('_col', '_mex'))
+    df = df[["alpha3", "code_col", "name_es_col", "name", "continent_code"]]
 
-    assert df.loc[192, "name"] is pd.np.nan
-    df.loc[192, "name"] = "Serbia and Montenegro"
+    missing = pd.read_table("../DANE/in/Colombia_countries_not_in_mexico.txt", encoding = "utf-16")
+    missing = missing[["alpha3", "code_col", "name_es_col", "name", "continent_code"]]
 
-    assert df.loc[249, "name"] is pd.np.nan
-    df.loc[249, "name"] = "Not Declared"
-
-    assert df.loc[250, "name"] is pd.np.nan
-    df.loc[250, "name"] = "Special Economic Zones"
-
-    df.name = df.name.astype(unicode)
-
-    h = Hierarchy(["country"])
-
+    df = pd.concat([df, missing])
+    df = df[~df.continent_code.isnull()].reset_index(drop = True)
+    
+    df["level"] = "country"
+    
+    df = df.rename(columns={
+        "code_col": "code",
+        "name_es_col": "name_es",
+        "continent_code": "parent_code"
+        })
+        
+    assert df.loc[6, "name"] is pd.np.nan
+    df.loc[6, "name"] = u"Netherlands Antilles"
+    
+    regions = pd.read_table("./in/Mexico Country codes - continents - Continents - Regions.tsv", encoding="utf-8")
+    df = pd.concat([df, regions]).reset_index(drop=True)
+    
+    h = Hierarchy(["region", "country"])
     parent_id_table = parent_code_table_to_parent_id_table(df, h)
-
+    
     c = Classification(parent_id_table, h)
 
     c.to_csv("out/locations_international_dane.csv")
-    c.to_stata("out/locations_international_dane.dta")
+    #c.to_stata("out/locations_international_dane.dta")
