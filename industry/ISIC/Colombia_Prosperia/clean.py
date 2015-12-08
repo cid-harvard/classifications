06344c8e79e2265ebb91361b7291957fb44f0ec0
Mali Akmanalp <mali@akmanalp.com>
2015-12-08 12:13:11 -0500

    Update colombia industry classification to be non-nesting

--- industry/ISIC/Colombia_Prosperia/clean.py
+++ industry/ISIC/Colombia_Prosperia/clean.py
@@ -6,43 +6,30 @@ from classification import (Hierarchy,
                             Classification)
 if __name__ == "__main__":
 
-    df = pd.read_table("./in/col_industry_name_category_master - Sheet1.tsv", encoding="utf-8")
-    df.columns = ["class_code", "division_code", "section_code",
-                  "name_es_class", "", "name_short_es_class", "name_en_class",
-                  "name_short_en_class", "name_es_division", "",
-                  "name_short_es_division", "name_en_division",
-                  "name_short_en_division", "name_en_section",
-                  "name_es_section"]
-
-    df = df.drop_duplicates(["class_code", "division_code", "section_code"]).reset_index(drop=True)
+    df = pd.read_table("./in/col_industry_name_category_master - Hierarchy.tsv", encoding="utf-8")
 
     df.class_code = df.class_code.astype(int).astype(str).str.zfill(4)
     df.division_code = df.division_code.astype(int).astype(str).str.zfill(2)
-    df.section_code = df.section_code.astype(int).astype(str).str.zfill(2)
+    df.section_code = df.section_code.astype(int).astype(str).str.zfill(1)
+
+    names = pd.read_table("./in/col_industry_name_category_master - Names.tsv", encoding="utf-8")
+    names.loc[names.level == "section", "code"] = names.code.astype(str)
+    names.loc[names.level == "division", "code"] = names.code.astype(str).str.zfill(2)
+    names.loc[names.level == "class", "code"] = names.code.astype(str).str.zfill(4)
 
     h = Hierarchy(["section", "division", "class"])
     parent_code_table = repeated_table_to_parent_id_table(
         df, h,
         level_fields={
-            "section": ["name_en_section", "name_es_section"],
-            "division": ["name_es_division", "name_short_es_division",
-                         "name_en_division", "name_short_en_division"],
-            "class": ["name_es_class", "name_short_es_class",
-                      "name_en_class", "name_short_en_class"],
-        })
+            "section": [],
+            "division": [],
+            "class": [],
 
-    parent_code_table = parent_code_table\
-        .sort(["level", "code"])\
-        .drop_duplicates(["level", "code"])\
-        .reset_index(drop=True)
-
-    parent_code_table.name_short_es.fillna(parent_code_table.name_es, inplace=True)
-    parent_code_table.name_short_en.fillna(parent_code_table.name_en, inplace=True)
-    parent_code_table = parent_code_table.reset_index(drop=True)
+        }).sort_values(by=["level", "code"])
 
     parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
+    parent_id_table = parent_id_table.merge(names)
 
-    parent_id_table.code = parent_id_table.code.astype(str)
     parent_id_table["name"] = parent_id_table.name_en
 
     c = Classification(parent_id_table, h)
