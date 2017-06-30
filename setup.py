3af2b1a9954fecaafd2e600703aecef1b71ab971
Mali Akmanalp <mali@akmanalp.com>
2017-06-29 20:55:07 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="linnaeus",
-    version="v0.0.74",
+    version="v0.0.76",
     author="Mali Akmanalp <Harvard CID>",
     description=("Harvard CID's classification tools."),
     url="http://github.com/cid-harvard/classifications/",
