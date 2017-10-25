432c40bafc8c7557a2c9dae343168e157c91183d
Mali Akmanalp <mali@akmanalp.com>
2017-10-25 16:27:31 -0400

    Update SITC rev 2 with new ids that have spacing between them in case it becomes needed in the future.

--- product/SITC/IntlAtlas/clean_sitc.py
+++ product/SITC/IntlAtlas/clean_sitc.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from classification import (Hierarchy, repeated_table_to_parent_id_table,
                             parent_code_table_to_parent_id_table,
+                            spread_out_entries, sort_by_code_and_level,
                             Classification)
 
 if __name__ == "__main__":
@@ -25,14 +26,26 @@ if __name__ == "__main__":
     h = Hierarchy(["section", "2digit", "3digit", "4digit"])
     parent_code_table = repeated_table_to_parent_id_table(hierarchy, h, fields)
     parent_code_table.code = parent_code_table.code.astype(str)
-
     parent_code_table = parent_code_table.merge(names, on=["code", "level"])
+
+    # Sort by level order (not necessarily alphabetical)
+    parent_code_table = sort_by_code_and_level(parent_code_table, h)
 
     parent_id_table = parent_code_table_to_parent_id_table(parent_code_table, h)
     parent_id_table["name"] = parent_id_table.name_en
 
     parent_id_table = parent_id_table[["code", "name", "level", "name_en",
                                        "name_es", "name_short_en", "name_short_es", "parent_id"]]
+
+    # Decide what id each level should start from
+    # Put ample space between each range of ids
+    level_starts = {
+        "section": 0,
+        "2digit": 100,
+        "3digit": 250,
+        "4digit": 650
+    }
+    parent_id_table = spread_out_entries(parent_id_table, level_starts, h)
 
     c = Classification(parent_id_table, h)
 
