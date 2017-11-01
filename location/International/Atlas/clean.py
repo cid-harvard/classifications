35ee06d66b4955def9676b7cf10b780ba4ecebb4
Mali Akmanalp <mali@akmanalp.com>
2017-11-01 17:21:36 -0400

    Add flags for in atlas and in country list.

--- location/International/Atlas/clean.py
+++ location/International/Atlas/clean.py
@@ -32,6 +32,12 @@ if __name__ == "__main__":
     parent_id_table = parent_id_table.reset_index(drop=True).set_index("new_index")
     parent_id_table.index.name = None
 
+    # Add in other properties, like in_book and trusted
+    trusted = pd.read_csv("./in/trusted_countries.csv", index_col="iso")
+    parent_id_table = parent_id_table.merge(trusted, left_on="code", right_index=True, how="left")
+    in_book = pd.read_table("./in/list_in_atlas_book.csv", index_col="iso", sep=";")
+    parent_id_table = parent_id_table.merge(in_book, left_on="code", right_index=True, how="left")
+
     c = Classification(parent_id_table, h)
     c.to_csv("out/locations_international_atlas.csv")
     c.to_stata("out/locations_international_atlas.dta")
