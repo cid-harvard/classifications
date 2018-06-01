ad33ea331f6dbd164f90eb3986b8be50a6151984
Mali Akmanalp <mali@akmanalp.com>
2018-06-01 11:30:10 -0400

    Remove Syria from rankings.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="linnaeus",
-    version="v0.0.99",
+    version="v0.0.100",
     author="Mali Akmanalp <Harvard CID>",
     description=("Harvard CID's classification tools."),
     url="http://github.com/cid-harvard/classifications/",
