c090619fccd85dbdf167fd1fa349c994ed67a359
Mali Akmanalp <mali@akmanalp.com>
2018-06-12 12:06:04 -0400

    Un-revert b3986a5 except for services and 6digit.

--- location/International/Atlas/clean.py
+++ location/International/Atlas/clean.py
@@ -32,11 +32,11 @@ if __name__ == "__main__":
     parent_id_table = parent_id_table.reset_index(drop=True).set_index("new_index")
     parent_id_table.index.name = None
 
-    # Add in other properties, like in_book and trusted
+    # Add in other properties, like in_rankings and trusted
     trusted = pd.read_csv("./in/trusted_countries.csv", index_col="iso")
     parent_id_table = parent_id_table.merge(trusted, left_on="code", right_index=True, how="left")
-    in_book = pd.read_table("./in/list_in_atlas_book.csv", index_col="iso", sep=";")
-    parent_id_table = parent_id_table.merge(in_book, left_on="code", right_index=True, how="left")
+    in_rankings = pd.read_table("./in/in_rankings.csv", index_col="iso", sep=";")
+    parent_id_table = parent_id_table.merge(in_rankings, left_on="code", right_index=True, how="left")
 
     c = Classification(parent_id_table, h)
     c.to_csv("out/locations_international_atlas.csv")
