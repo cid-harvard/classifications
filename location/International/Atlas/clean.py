aed2d617972ba3979d91ee9c8ab5be4c0d971f89
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2018-09-07 11:50:27 -0400

    Remove World as a country in Atlas classification

--- location/International/Atlas/clean.py
+++ location/International/Atlas/clean.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-from classification import (Hierarchy, parent_code_table_to_parent_id_table,
-                            Classification)
+from classification import (
+    Hierarchy,
+    parent_code_table_to_parent_id_table,
+    Classification,
+)
 
 if __name__ == "__main__":
 
-    df = pd.read_csv("./in/International Atlas Location Codes - Clean Countries.csv",
-                     encoding="utf-8",
-                     dtype={"parent_code": str}
-                     )\
-        .drop("comtrade_name", axis=1)
+    df = pd.read_csv(
+        "./in/International Atlas Location Codes - Clean Countries.csv",
+        encoding="utf-8",
+        dtype={"parent_code": str},
+    ).drop("comtrade_name", axis=1)
     df["level"] = "country"
 
-    regions = pd.read_csv("./in/International Atlas Location Codes - Continents and Regions.csv", encoding="utf-8")
+    regions = pd.read_csv(
+        "./in/International Atlas Location Codes - Continents and Regions.csv",
+        encoding="utf-8",
+    )
     regions["name_short_en"] = regions["name_en"]
     regions["name_short_es"] = regions["name_es"]
     regions["level"] = "region"
@@ -34,16 +40,25 @@ if __name__ == "__main__":
 
     # Add in other properties, like in_rankings and trusted
     trusted = pd.read_csv("./in/trusted_countries.csv", index_col="iso")
-    parent_id_table = parent_id_table.merge(trusted, left_on="code", right_index=True, how="left")
+    parent_id_table = parent_id_table.merge(
+        trusted, left_on="code", right_index=True, how="left"
+    )
     in_rankings = pd.read_table("./in/in_rankings.csv", index_col="iso", sep=";")
-    parent_id_table = parent_id_table.merge(in_rankings, left_on="code", right_index=True, how="left")
+    parent_id_table = parent_id_table.merge(
+        in_rankings, left_on="code", right_index=True, how="left"
+    )
     services = pd.read_csv("./in/services_flags.csv", index_col="code")
     services = services.astype(float)
-    parent_id_table = parent_id_table.merge(services, left_on="code", right_index=True, how="left")
+    parent_id_table = parent_id_table.merge(
+        services, left_on="code", right_index=True, how="left"
+    )
 
     # Services flags should be False in case of regions, per Huy's request
     parent_id_table.loc[parent_id_table.level == "region", "reported_serv"] = 0
     parent_id_table.loc[parent_id_table.level == "region", "reported_serv_recent"] = 0
+
+    # Remove World as a country since it doesn't have data
+    parent_id_table = parent_id_table[parent_id_table.code != "WLD"]
 
     c = Classification(parent_id_table, h)
     c.to_csv("out/locations_international_atlas.csv")
