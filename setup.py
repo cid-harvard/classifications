e42ed957905b88ed16dc6ca447e0c26056dc33e5
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2019-06-18 11:54:46 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="linnaeus",
-    version="v112",
+    version="v113",
     author="Mali Akmanalp <Harvard CID>",
     description=("Harvard CID's classification tools."),
     url="http://github.com/cid-harvard/classifications/",
