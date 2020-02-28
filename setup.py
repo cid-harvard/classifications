89795aa3f0e8d710000e2344e67b22fcf1e38fd8
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-02-28 15:14:03 -0500

    skipped v114

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="linnaeus",
-    version="v115",
+    version="v114",
     author="Mali Akmanalp <Harvard CID>",
     description=("Harvard CID's classification tools."),
     url="http://github.com/cid-harvard/classifications/",
